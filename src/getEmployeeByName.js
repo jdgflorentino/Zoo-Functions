ef39850ee8cead9d3684e386bb4d8b10f2840ebc
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-25 19:53:22 -0300

    Requisito 3.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const employee = (nome) => nome.firstName === employeeName || nome.lastName === employeeName;
+  // chamei uma funcao dentro do find pois a linha estava muito longa e foi o melhor jeito de verificar os nomes.
+  const employeeFind = data.employees.find(employee);
+  return employeeFind === undefined ? {} : employeeFind;
 }
+
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
