c2672df9daa612f59a17ac9d634fb56500448cf8
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-25 18:54:07 -0300

    Requisito 2.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // variável com os todos os residents.
+  const animalFind = data.species.find((nomeAnimal) => nomeAnimal.name === animal).residents;
+  // usei .every pois ele retorna boolean.
+  return animalFind.every((element) => element.age >= age);
 }
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  console.log(...ids);
   return ids.map((id) => data.species.find((element) => element.id === id));
 }
 
