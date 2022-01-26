c4bdac4734d21e5126ae21cdfb66549cdaafa341
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-26 15:09:02 -0300

    Requisito 5.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employees) => employees.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    // fiz a variavel allEmployees pois a linha com o filter() estava com length > 100
+    const allEmployees = data.employees;
+    // eFilter = array com todas as infos dos employees geridos pelo manager passado como parametro.
+    const eFilter = allEmployees.filter((employees) => employees.managers.includes(managerId));
+    // callback para concatenar nome e sobrenome dos employees e map() retorna um array com a união dessas chaves.
+    return eFilter.map((employees) => `${employees.firstName} ${employees.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
