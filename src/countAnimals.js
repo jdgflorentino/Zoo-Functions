70b05a9114da76be8d9575c0b5c998c4525b79b2
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-26 17:43:46 -0300

    Requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const allAnimals = data.species;
+  if (!animal) {
+    const quantAnimal = allAnimals.map((animals) => ({ [animals.name]: animals.residents.length }));
+    return Object.assign(...quantAnimal);
+  }
+  const keys = Object.keys(animal);
+  const values = Object.values(animal);
+  const animalsName = (animals) => animals.name === values[0];
+  if (keys.includes('sex')) {
+    return allAnimals.find(animalsName).residents
+      .filter((animals) => animals.sex === values[1]).length;
+  }
+  return allAnimals.find(animalsName).residents.length;
 }
 
 module.exports = countAnimals;
