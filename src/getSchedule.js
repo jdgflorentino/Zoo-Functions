8134e83b423adf60c14ca70752c243ca25623880
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-27 15:38:39 -0300

    Requisito 8.5

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+  // separar em partes
+  // o que consigo fazer primeiro? 1 puxar o array com os dias quando o parametro for animal e fazer o Monday.
 
+  const allAnimals = species.map((animal) => animal.name);
+  if (allAnimals.includes(scheduleTarget)) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
+  }
+  // preparar officeHour, preciso fazer template literals com os objetos de cada dia.
+  // preparar exhibition filtrando os animais que estão no dia
+  // const allDays = Object.keys(hours);
+  // if (allDays.includes(scheduleTarget)) {
+  //   return { scheduleTarget: { officeHour, exhibition } };
+  // }
+}
+console.log(getSchedule('lions'));
 module.exports = getSchedule;
