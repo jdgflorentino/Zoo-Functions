075047b4398cea263286df5f6ab29a5b29611dcd
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-27 19:48:56 -0300

    Requisito 8 com Object Destructuring.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,14 @@
 const { species, hours } = require('../data/zoo_data');
 
 const allDays = Object.keys(hours);
-
-const allAnimals = species.map((animal) => animal.name);
+const allAnimals = species.map(({ name }) => name);
 
 const frase = (day) => `Open from ${hours[day].open}am until ${hours[day].close}pm`;
-
-const animais = (day) => species.filter((animal) => animal.availability.includes(day))
-  .map((animal) => animal.name);
-
-const obj = {};
+const animais = (day) => species.filter(({ availability }) => availability.includes(day))
+  .map(({ name }) => name);
 
 const defaultTest = () => {
+  const obj = {};
   allDays.forEach((day) => {
     obj[day] = {
       officeHour: frase(day),
@@ -26,21 +23,14 @@ const defaultTest = () => {
 };
 
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget === 'Monday') {
-    return { Monday: { officeHour: 'CLOSED',
-      exhibition: 'The zoo will be closed!' } };
-  }
-
   if (allAnimals.includes(scheduleTarget)) {
-    return species.find((animal) => animal.name === scheduleTarget).availability;
+    return species.find(({ name }) => name === scheduleTarget).availability;
   }
 
   if (allDays.includes(scheduleTarget)) {
-    return { [scheduleTarget]: { officeHour: frase(scheduleTarget),
-      exhibition: animais(scheduleTarget) } };
+    return { [scheduleTarget]: defaultTest()[scheduleTarget] };
   }
 
   return defaultTest();
 }
-console.log(getSchedule());
 module.exports = getSchedule;
